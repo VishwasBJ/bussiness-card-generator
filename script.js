51072a7850a15ce2dffad7b20c9e66b61f64cbde
VishwasBJ <vishwasjai2004@gmail.com>
2024-08-10 22:43:13 +0530

    first try
    this may be wrong

---
+++ script.js
@@ -0,0 +1,30 @@
+let a=prompt("Enter any number from 1 to 3");
+if(a==1){
+    console.log("Crazy")
+}
+if(a==2){
+    console.log("Amazing")
+}
+if(a==3){
+    console.log("Fire")
+}
+let b=prompt("Enter any number from 1 to 3");
+if(b==1){
+    console.log("Engine")
+}
+if(b==2){
+    console.log("Foods")
+}
+if(b==3){
+    console.log("Garments")
+}
+let c=prompt("Enter any number from 1 to 3");
+if(c==1){
+    console.log("Bros")
+}
+if(c==2){
+    console.log("Limited")
+}
+if(c==3){
+    console.log("Hub")
+}
